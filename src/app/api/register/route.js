d1792702699703cd7015583cbd447ada16e4eeae
Jeon.c.w <33561661+jeon9897@users.noreply.github.com>
2025-06-19 12:42:52 +0900

    Add files via upload

---
+++ src/app/api/register/route.js
@@ -0,0 +1,32 @@
+import {db} from '@/lib/db';
+import bcrypt from 'bcrypt';
+
+export async function POST(req){
+  //1. 변수선언
+  const {username, password} = await req.json();
+
+  //2. 넘겨받은 데이터 빈 데이터 없는지...
+  if(!username || !password){
+    return new Response(JSON.stringify({message:'필수 항목 누락'}),{
+      status:400
+    });
+  }
+
+  //3. 넘겨받은 데이터가 일치하는게 있는지
+  const [existing] = await db.query('SELECT * FROM users WHERE username=?', [username]);
+
+  if(existing.length>0){
+    return new Response(JSON.stringify({message:'이미 사용중인 사용자입니다.'}),
+  {status:409});
+  }
+
+  //4. 일치하는 데이터가 없다면 쿼리문으로 데이터 입력하여 회원정보추가
+  //패스워드 암호화
+  const hashedPassword = await bcrypt.hash(password, 10); 
+
+  await db.query('INSERT INTO users (username, password) VALUES (?,?)', [username, hashedPassword]);
+
+  return new Response(JSON.stringify({message:'회원가입이 완료되었습니다.'}),{
+    status:201
+  });
+}
