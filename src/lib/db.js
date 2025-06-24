a1f5057e00233f150bd7358bdd5cc44054333d67
Jeon.c.w <33561661+jeon9897@users.noreply.github.com>
2025-06-24 17:06:26 +0900

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 //db연결정보
 export const db = mysql.createPool({
   host:process.env.DB_HOST,
-  port:process.env.DB_PORT || 31573,
+  port:Number(process.env.DB_PORT),
   user:process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
